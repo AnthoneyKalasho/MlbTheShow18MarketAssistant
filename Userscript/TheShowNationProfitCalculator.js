e3f3946fe14e23f6f680391af76e20e8081309be
cardona150 <575891+cardona150@users.noreply.github.com>
2018-04-10 13:32:44 -0700

    Extensionize
    added chrome extension version, you will have to go to chrome://extensions/, enable developer mode and pack the $\MlbTheShow18MarketAssistant\extension directory. Main scipt in here is copy paste from the userscript file so they could easily get out of sync. Also fixed some more date parsing. Visual studio reformatted everything so this looks like a lot more changes than it is.

--- Userscript/TheShowNationProfitCalculator.js
+++ Userscript/TheShowNationProfitCalculator.js
@@ -1,12 +1,3 @@
-// ==UserScript==
-// @name         MLB The Show Nation Profit Calculator
-// @namespace    https://greasyfork.org/en/users/177153-anthoney-kalasho
-// @version      2018.4.5.2
-// @description  Calculates the current profitability of a card and displays it on search page
-// @author       AnthoneyKalasho
-// @match        https://mlb18.theshownation.com/community_market/listings*
-// @grant        unsafeWindow
-// ==/UserScript==
 $(document).ready(function () {
     'use strict';
     // "Market prices on this page are refreshed hourly." Not anymore!!! :D
@@ -24,7 +15,7 @@ $(document).ready(function () {
     });
     // Make room for MLB The Show 18 Market Assistant supertable
     $('.marketplace-filter-list').remove();
-    $(objectArray).each( function () {
+    $(objectArray).each(function () {
         tableHTML += '<tr><td class="nameRow"><a href="' + $(this)[0].url + '">' + $(this)[0].name + '</a></td><td class="buyNowRow">-</td><td class="sellNowRow">-</td><td class="datRow">-</td><td class="pdRow">-</td><td class="soldInHourRow">-</td></tr>';
     });
     tableHTML += '</tbody></table>';
@@ -41,19 +32,20 @@ $(document).ready(function () {
         var pdRow = $(this).find('.pdRow');
         var soldInHourRow = $(this).find('.soldInHourRow');
         soldInHourRow.html(0);
-        $.ajax({url: rowURL
-               }).done(function (data) {
+        $.ajax({
+            url: rowURL
+        }).done(function (data) {
             var parsedData = $(jQuery.parseHTML(data));
             var buyNowButtonContainer = parsedData.find('.marketplace-card-buy-orders > .marketplace-card-create-forms > .marketplace-card-order-now')[0];
             // If somebody is offering to sell this item, get price
-            if(buyNowButtonContainer!=null){mBuyNowPrice = buyNowButtonContainer.innerText.replace(/\D/g, '');}
+            if (buyNowButtonContainer != null) { mBuyNowPrice = buyNowButtonContainer.innerText.replace(/\D/g, ''); }
             // Don't have to do this but it makes console errors go away
-            if(mBuyNowPrice != null){buyNowRow.html(mBuyNowPrice);}
+            if (mBuyNowPrice != null) { buyNowRow.html(mBuyNowPrice); }
             var sellNowButtonContainer = parsedData.find('.marketplace-card-sell-orders > .marketplace-card-create-forms > .marketplace-card-order-now')[0];
             // If somebody is offering to buy this item, get price
-            if(sellNowButtonContainer!=null){mSellNowPrice = sellNowButtonContainer.innerText.replace(/\D/g, '');}
+            if (sellNowButtonContainer != null) { mSellNowPrice = sellNowButtonContainer.innerText.replace(/\D/g, ''); }
             // Don't have to do this but it makes console errors go away
-            if(mSellNowPrice != null){sellNowRow.html(mSellNowPrice);}
+            if (mSellNowPrice != null) { sellNowRow.html(mSellNowPrice); }
             parsedData.find('.completed-order').each(function () {
                 var thisDate = getMlbtsDateFromString($(this).find('.date').text());
                 var OneHourAgo = new Date(Date.now());
@@ -70,14 +62,13 @@ $(document).ready(function () {
             soldInHourRow.html(mSoldInHour);
             // mBuyNowPrice can be null if nobody is offering to sell this item
             // mSellNowPrice can be null if nobody is offering to buy this item
-            if(mBuyNowPrice != null && mSellNowPrice !=null)
-            {
+            if (mBuyNowPrice != null && mSellNowPrice != null) {
                 var mDifAfterTax = (mBuyNowPrice - (mBuyNowPrice * 0.1)) - mSellNowPrice;
                 mDifAfterTax = precisionRound(mDifAfterTax, 2);
                 datRow.html(mDifAfterTax);
                 var mPercentDiff = (mDifAfterTax / mSellNowPrice) * 100;
                 mPercentDiff = precisionRound(mPercentDiff, 2);
-                pdRow.html(mPercentDiff+'%');
+                pdRow.html(mPercentDiff + '%');
             }
         });
     });
@@ -85,25 +76,22 @@ $(document).ready(function () {
 
 // Chrome can't convert the date format in the Completed Orders section of an item's page into a Date
 // This metod will reformat the string properly and the turn it into a Date object
-function getMlbtsDateFromString(dateString)
-{
-  var date = new Date();
-  var fixedDateString;
-  if(dateString.substring(20,21)=='P')
-  {
-    // If PM, add 12 hours and cut off the end bit that keeps dateString from deserializing properly
-    var hour = parseInt(dateString.substring(15,17));
-    hour += 12;
-    fixedDateString = dateString.slice(0,15) + hour + dateString.slice(17,20);
-    date = Date(fixedDateString);
-  }
-  else
-  {
-    // If AM just cut off the end bit that keeps dateString from deserializing properly
-    fixedDateString = dateString.slice(0,20);
-    date = new Date(fixedDateString);
-  }
-  return date;
+function getMlbtsDateFromString(dateString) {
+    var date;
+    var fixedDateString;
+    if (dateString.substring(20, 21) == 'P') {
+        // If PM, add 12 hours and cut off the end bit that keeps dateString from deserializing properly
+        var hour = parseInt(dateString.substring(15, 17));
+        hour += 12;
+        fixedDateString = dateString.slice(0, 15) + hour + dateString.slice(17, 20);
+        date = new Date(fixedDateString);
+    }
+    else {
+        // If AM just cut off the end bit that keeps dateString from deserializing properly
+        fixedDateString = dateString.slice(0, 20);
+        date = new Date(fixedDateString);
+    }
+    return date;
 }
 
 function precisionRound(number, precision) {
---
+++ extension/script.js
@@ -0,0 +1,98 @@
+'use strict';
+// "Market prices on this page are refreshed hourly." Not anymore!!! :D
+$('.marketplace-filter-disclaimer').remove();
+var tableHTML = '<table class="sortable" width= 100%; font-size: 15px;"><thead><tr><th>Name</th><th class="sorttable_numeric">Buy Now</th><th class="sorttable_numeric">Sell Now</th><th class="sorttable_numeric">Profit(After Tax)</th><th class="sorttable_numeric">Difference% (After Tax)</th><th class="sorttable_numeric">Bought/Sold in the past hour</th></thead></tr><tbody>';
+var objectArray = [];
+$('.marketplace-filter-item').each(function () {
+    var name = $(this).find('.marketplace-filter-item-name > a').text();
+    var url = $(this).find('.marketplace-filter-item-name > a').attr('href');
+    var row = {
+        url: url,
+        name: name,
+    };
+    objectArray.push(row);
+});
+// Make room for MLB The Show 18 Market Assistant supertable
+$('.marketplace-filter-list').remove();
+$(objectArray).each(function () {
+    tableHTML += '<tr><td class="nameRow"><a href="' + $(this)[0].url + '">' + $(this)[0].name + '</a></td><td class="buyNowRow">-</td><td class="sellNowRow">-</td><td class="datRow">-</td><td class="pdRow">-</td><td class="soldInHourRow">-</td></tr>';
+});
+tableHTML += '</tbody></table>';
+$('.menu-pagination').before('<script type="text/javascript" src="https://www.kryogenix.org/code/browser/sorttable/sorttable.js">');
+$('.menu-pagination').before(tableHTML);
+$('.sortable > tbody  > tr').each(function () {
+    var mBuyNowPrice;
+    var mSellNowPrice;
+    var mSoldInHour = 0;
+    var rowURL = $(this).find('.nameRow > a').attr('href');
+    var buyNowRow = $(this).find('.buyNowRow');
+    var sellNowRow = $(this).find('.sellNowRow');
+    var datRow = $(this).find('.datRow');
+    var pdRow = $(this).find('.pdRow');
+    var soldInHourRow = $(this).find('.soldInHourRow');
+    soldInHourRow.html(0);
+    $.ajax({
+        url: rowURL
+    }).done(function (data) {
+        var parsedData = $(jQuery.parseHTML(data));
+        var buyNowButtonContainer = parsedData.find('.marketplace-card-buy-orders > .marketplace-card-create-forms > .marketplace-card-order-now')[0];
+        // If somebody is offering to sell this item, get price
+        if (buyNowButtonContainer != null) { mBuyNowPrice = buyNowButtonContainer.innerText.replace(/\D/g, ''); }
+        // Don't have to do this but it makes console errors go away
+        if (mBuyNowPrice != null) { buyNowRow.html(mBuyNowPrice); }
+        var sellNowButtonContainer = parsedData.find('.marketplace-card-sell-orders > .marketplace-card-create-forms > .marketplace-card-order-now')[0];
+        // If somebody is offering to buy this item, get price
+        if (sellNowButtonContainer != null) { mSellNowPrice = sellNowButtonContainer.innerText.replace(/\D/g, ''); }
+        // Don't have to do this but it makes console errors go away
+        if (mSellNowPrice != null) { sellNowRow.html(mSellNowPrice); }
+        parsedData.find('.completed-order').each(function () {
+            var thisDate = getMlbtsDateFromString($(this).find('.date').text());
+            var OneHourAgo = new Date(Date.now());
+            OneHourAgo.setHours(OneHourAgo.getHours() - 1);
+            // These come in descending order
+            if (thisDate > OneHourAgo) {
+                mSoldInHour++;
+            }
+            else {
+                // Stop as soon as you find one that is more than an hour ago
+                return false;
+            }
+        });
+        soldInHourRow.html(mSoldInHour);
+        // mBuyNowPrice can be null if nobody is offering to sell this item
+        // mSellNowPrice can be null if nobody is offering to buy this item
+        if (mBuyNowPrice != null && mSellNowPrice != null) {
+            var mDifAfterTax = (mBuyNowPrice - (mBuyNowPrice * 0.1)) - mSellNowPrice;
+            mDifAfterTax = precisionRound(mDifAfterTax, 2);
+            datRow.html(mDifAfterTax);
+            var mPercentDiff = (mDifAfterTax / mSellNowPrice) * 100;
+            mPercentDiff = precisionRound(mPercentDiff, 2);
+            pdRow.html(mPercentDiff + '%');
+        }
+    });
+});
+
+// Chrome can't convert the date format in the Completed Orders section of an item's page into a Date
+// This metod will reformat the string properly and the turn it into a Date object
+function getMlbtsDateFromString(dateString) {
+    var date;
+    var fixedDateString;
+    if (dateString.substring(20, 21) == 'P') {
+        // If PM, add 12 hours and cut off the end bit that keeps dateString from deserializing properly
+        var hour = parseInt(dateString.substring(15, 17));
+        hour += 12;
+        fixedDateString = dateString.slice(0, 15) + hour + dateString.slice(17, 20);
+        date = new Date(fixedDateString);
+    }
+    else {
+        // If AM just cut off the end bit that keeps dateString from deserializing properly
+        fixedDateString = dateString.slice(0, 20);
+        date = new Date(fixedDateString);
+    }
+    return date;
+}
+
+function precisionRound(number, precision) {
+    var factor = Math.pow(10, precision);
+    return Math.round(number * factor) / factor;
+}
